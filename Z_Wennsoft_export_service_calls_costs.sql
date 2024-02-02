f07c44e1e145f50a8d65403c7858ad3309d38211
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-02-02 10:17:38 -0500

    Update Z_Wennsoft_export_service_calls_costs.sql
    Updated cost code assignments and filters.

--- Z_Wennsoft_export_service_calls_costs.sql
+++ Z_Wennsoft_export_service_calls_costs.sql
@@ -11,7 +11,7 @@ SELECT
 		WHEN Costs.Sp_Cost_Code = 'E' THEN '1'
 		WHEN Costs.Sp_Cost_Code = 'M' THEN '2'
 		WHEN Costs.Sp_Cost_Code = 'S' THEN '4'
-		WHEN Costs.Sp_Cost_Code = 'O' THEN '5'
+		WHEN Costs.Sp_Cost_Code = 'O' THEN '4'
 		WHEN Costs.Sp_Cost_Code = 'L' THEN '6'
 		ELSE Costs.Sp_Cost_Code
 	END AS WS_Cost_Code
@@ -26,7 +26,7 @@ SELECT
 		ELSE '0'
 	END AS TRX_Qty
 	,'' AS Unit_Cost				-- Calculated in Excel
-	,'' AS Extended_Cost			-- Calculated in Excel
+	,'' AS Extended_Cost				-- Calculated in Excel
 	,Costs.Transaction_Amount AS Billing_Amount
 	,Costs.Pay_Type AS Pay_Record
 	,Costs.Tran_Source AS TRX_Source
@@ -42,3 +42,7 @@ FROM
 	,LTRIM(RTRIM(AP_Vendor_Code)) AS Vendor_Code
 	FROM WO_COST_HISTORY_MC) AS Costs
 		ON Calls.WO_Number = Costs.WO_Number
+
+WHERE
+	Costs.Sp_Cost_Code <> ''
+	AND Calls.Status <> 'Closed'
