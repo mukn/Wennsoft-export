ee09a681a40a30cffa65017c3c6609c78441ac73
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-03-04 08:38:43 -0500

    Update Z_Wennsoft_export_locations_union.sql
    Repair the comparison value.

--- Z_Wennsoft_export_locations_union.sql
+++ Z_Wennsoft_export_locations_union.sql
@@ -1,16 +1,16 @@
 /* Z_Wennsoft_export_locations_union */ 
 SELECT Customer_Code, Contract_Number, Location_Code, Location_Name, Location_Addr1, Location_Addr2, Location_City, Location_State, 
-                         Location_Zip, Location_Phone1, Location_Phone2, Location_Contact, Service_Zone, [Contract/Non-contract], Vertical_Market, Sales_Tax_Code, Comparison_Key, Location_BillTo, Customer_Email
+                         Location_Zip, Location_Phone1, Location_Phone2, Location_Contact, Service_Zone, [Contract/Non-contract], Vertical_Market, Sales_Tax_Code, { fn CONCAT(Customer_Code, { fn CONCAT('-', Location_Code) }) } AS Second_Comparison_Key, Comparison_Key, Location_BillTo, Customer_Email
 FROM         Z_Wennsoft_export_locations_contracts_billto
 UNION
 SELECT   Customer_Code, Contract_Number, Location_Code, Location_Name, Location_Addr1, Location_Addr2, Location_City, Location_State, Location_Zip, Location_Phone1, Location_Phone2, 
-                         Location_Contact, Service_Zone, [Contract/Non-contract], Vertical_Market, Sales_Tax_Code, Comparison_Key, Location_BillTo, Customer_Email
+                         Location_Contact, Service_Zone, [Contract/Non-contract], Vertical_Market, Sales_Tax_Code, { fn CONCAT(Customer_Code, { fn CONCAT('-', Location_Code) }) } AS Second_Comparison_Key, Comparison_Key, Location_BillTo, Customer_Email
 FROM         Z_Wennsoft_export_locations_contracts_work_locations
 UNION
 SELECT   Customer_Code, Job_Number, Location_Code, Location_Name, Location_Addr1, Location_Addr2, Location_City, Location_State, Location_Zip, Location_Phone1, Location_Phone2, 
-                         Location_Contact, Service_Zone, [Contract/Non-contract], Vertical_Market, Sales_Tax_Code, Comparison_Key, Location_BillTo, Customer_Email
+                         Location_Contact, Service_Zone, [Contract/Non-contract], Vertical_Market, Sales_Tax_Code, { fn CONCAT(Customer_Code, { fn CONCAT('-', Location_Code) }) } AS Second_Comparison_Key, Comparison_Key, Location_BillTo, Customer_Email
 FROM         Z_Wennsoft_export_locations_jobs_work_locations
 UNION
 SELECT   Customer_Code, Job_Number, Location_Code, Location_Name, Location_Addr1, Location_Addr2, Location_City, Location_State, Location_Zip, Location_Phone1, Location_Phone2, 
-                         Location_Contact, Service_Zone, [Contract/Non-contract], Vertical_Market, Sales_Tax_Code, Comparison_Key, Location_BillTo, Customer_Email
+                         Location_Contact, Service_Zone, [Contract/Non-contract], Vertical_Market, Sales_Tax_Code, { fn CONCAT(Customer_Code, { fn CONCAT('-', Location_Code) }) } AS Second_Comparison_Key, Comparison_Key, Location_BillTo, Customer_Email
 FROM         Z_Wennsoft_export_locations_jobs_billto
