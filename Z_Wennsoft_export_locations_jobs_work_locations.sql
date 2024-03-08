cb80295feb106b6457b448072f8eed18a1965017
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-03-08 09:27:35 -0500

    Update Z_Wennsoft_export_locations_jobs_work_locations.sql
    Corrected latitude and longitude.

--- Z_Wennsoft_export_locations_jobs_work_locations.sql
+++ Z_Wennsoft_export_locations_jobs_work_locations.sql
@@ -26,14 +26,14 @@ SELECT
   END AS Location_BillTo
   ,{ fn CONCAT(j1.Customer_Code, { fn CONCAT('-', { fn CONCAT(j1.WO_Site, '-Main') }) }) } AS Comparison_Key
   ,Cust.Customer_Email
-  ,Site.Longitude
-  ,Site.Latitude
+  ,j1.Longitude
+  ,j1.Latitude
 FROM         
   (SELECT LTRIM(RTRIM(Customer_Code)) AS Customer_Code, LTRIM(RTRIM(Job_Number)) AS Job_Number, LTRIM(RTRIM(Job_Description)) AS Job_Description, LTRIM(RTRIM(Address_1)) AS Address_1, LTRIM(RTRIM(Address_2)) AS Address_2, LTRIM(RTRIM(City)) AS City, LTRIM(RTRIM(State)) AS State, LTRIM(RTRIM(Zip_Code)) 
-                                                    AS Zip_Code, LTRIM(RTRIM(Phone)) AS Phone, LTRIM(RTRIM(Sales_Tax_Code)) AS Sales_Tax_Code, LTRIM(RTRIM(WO_Site)) AS WO_Site
+                                                    AS Zip_Code, LTRIM(RTRIM(Phone)) AS Phone, LTRIM(RTRIM(Sales_Tax_Code)) AS Sales_Tax_Code, LTRIM(RTRIM(WO_Site)) AS WO_Site, Latitude, Longitude
   FROM dbo.JC_JOB_MASTER_MC
   WHERE (Company_Code = 'NA2') AND (Status_Code = 'A')
-  GROUP BY Customer_Code, Job_Number, Job_Description, Address_1, Address_2, City, State, Zip_Code, Phone, Sales_Tax_Code, WO_Site) AS j1
+  GROUP BY Customer_Code, Job_Number, Job_Description, Address_1, Address_2, City, State, Zip_Code, Phone, Sales_Tax_Code, WO_Site, Latitude, Longitude) AS j1
   LEFT OUTER JOIN
   dbo.Z_K2A_EXPORT_CUSTOMERS as Cust
     ON j1.Customer_Code = Cust.Customer_Code
