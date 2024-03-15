8a541ccd81e694be3aba990f4b54cb5568c733a3
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-03-15 12:32:49 -0400

    Update Z_Wennsoft_export_purchase_orders.sql

--- Z_Wennsoft_export_purchase_orders.sql
+++ Z_Wennsoft_export_purchase_orders.sql
@@ -4,40 +4,49 @@ SELECT
 	,LTRIM(RTRIM(Det.Line_Number)) AS Line_Number			-- PO line number
 	,LTRIM(RTRIM(PO.Vendor_Code)) AS Vendor_Code			-- Vendor ID
 	,PO.PO_Date_List1 AS Document_Date						-- Document date
-	--,Line number
 	,1 AS PO_Type											-- PO type (always 1)
-	--,Product indicator (1=None, 2=Job cost, 3=Service)
+	,CASE
+		WHEN PO.Job_Number <> '' THEN 2
+		WHEN PO.WO_Number <> '' THEN 3
+		ELSE 1
+	END AS Product_indicator								--,Product indicator (1=None, 2=Job cost, 3=Service)
 	,CASE
 		WHEN PO.Job_Number <> '' THEN LTRIM(RTRIM(PO.Job_Number))
 		WHEN PO.WO_Number <> '' THEN LTRIM(RTRIM(PO.WO_Number))
 		ELSE ''
 	END AS Work_Number										-- Job number
-	--,Cost type
-	--,Cost code
-	--,PO line status
-	--,PO status
-	--,Qty order
-	--,Qty com td
-	--,Qty inv cd
-	--,Qty cance
-	--,Qty rej
-	--,Qty actual
-	--,Unit cost
+	,CASE
+		WHEN PO.Job_Number <> '' THEN 'n/a'
+		ELSE 'Service cost code needed'
+	END AS Cost_type										--,Cost type			Needs definition
+	,CASE
+		WHEN PO.WO_Number <> '' THEN 'n/a'
+		ELSE 'Job cost code needed'
+	END AS Cost_type										--,Cost code			Needs definition
+	,'1 or 2' AS PO_line_status								--,PO line status		Needs definition
+	,'1 or 2' AS PO_status									--,PO status			Needs definition
+	,Det.PO_Quantity_List1 AS Qty_Ordered					--,Qty order
+	,'qty if po status & line status = 2' AS Qty_comtd		--,Qty com td			Needs definition
+	,'0' AS Qty_inv_cd										--,Qty inv cd
+	,'0' AS Qty_cance										--,Qty cance
+	,'0' AS Qty_rej											--,Qty rej
+	,'0' AS Qty_actual										--,Qty actual
+	,Det.Item_Price											--,Unit cost
 	--,Retention pct
 	--,Retention amount TTD
 	--,Tax amnt
 	--,Frt amnt
 	--,Committed cost
-	--,Actual total cost
-	--,Ret unit cost
-	--,Ret total cost
-	--,Location code
-	--,VNDITNUM -- Vendor item number ?
-	--,Item number
-	--,Quantity
-	--,Non-inventory
+	,CASE
+		WHEN Det.PO_Quantity_List1 > 0 THEN Det.PO_Quantity_List1 * Det.Item_Price
+		ELSE 0
+	END AS Cost_Act_Total									--,Actual total cost
+	,'0' AS Cost_ret_unit --,Ret unit cost
+	,'0' AS Cost_rej_total --,Ret total cost
+	,'WAREHOUSE' AS Location_Code--,Location code
+	,Det.Item_Code AS Item_Number							--,Item number
 	,Det.Unit_Of_Measure AS UOFM							-- Unit of measure
-	--,Purchase IV item taxable
+	,'1, 2, or 3' AS Item_Taxable							--,Purchase IV item taxable				Needs definition
 	
 	
 	
@@ -45,23 +54,30 @@ SELECT
 	,PO.PO_Date_List2 AS Date_Delivery					-- Delivery date
 	,PO.PO_Date_List3 AS Date_Last_Received				-- Last received
 	
-	,Det.Item_Code
+	
 	,Det.Item_Description
-	,Det.Item_Discount_Percent
-	,Det.Item_Price
+	,Det.Item_Discount_Percent AS Item_Discount_Pct
+	
 	,Det.WO_Item_Price
 	
 	
-	,PO.Batch_Code
+	
+	--,PO.Batch_Code
 	,''
+	
+	,Det.PO_Quantity_List2 AS Qty_Received
+	,Det.PO_Quantity_List3
+	,Det.PO_Quantity_List4
+	,Det.PO_Quantity_List5
+	
 
 	--,PO.*
 FROM
 	PO_INQUIRY_V_MC AS PO WITH (NOLOCK)
 	LEFT JOIN
 	PO_PURCHASE_ORDER_DETAIL_MC AS Det WITH (NOLOCK)
-		ON PO.PO_Number = LTRIM(RTRIM(Det.PO_Number))
+		ON LTRIM(RTRIM(PO.PO_Number)) = LTRIM(RTRIM(Det.PO_Number))
 WHERE
 	PO.Company_Code = 'NA2'
-	AND PO.Status = 'Open'
-ORDER BY LTRIM(RTRIM(PO.PO_Number)) DESC
+	--AND PO.Status = 'Open'
+ORDER BY LTRIM(RTRIM(PO.PO_Number)) DESC, LTRIM(RTRIM(Det.Line_Number))
