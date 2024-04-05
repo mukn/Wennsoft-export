d36d8e8470ae1354ebafe31b9824dfb64c94dff1
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-04-05 10:18:19 -0400

    Create Z_Wennsoft_export_service_contracts_schedule.sql
    Initial commit.

---
+++ Z_Wennsoft_export_service_contracts_schedule.sql
@@ -0,0 +1,31 @@
+SELECT
+  Contracts.Customer_Code
+  ,Contracts.Contract_number
+  ,Contracts.Location_Work
+  ,Visit.Visit_Date
+  --,VEqp.Equipment
+  ,Visit.Budget_Hours
+  ,Visit.Description AS Description_Spectrum
+  ,CASE
+	WHEN Visit.Description LIKE 'Annual%Maint%' THEN 'Annual Maintenance'
+	WHEN Visit.Description LIKE 'Tower Cle%' THEN 'Annual Tower Cleaning'
+	WHEN Visit.Description LIKE '%Change%' THEN 'Changeover'
+	WHEN Visit.Description LIKE 'Engin%' THEN 'Engineering'
+	WHEN Visit.Description LIKE '%Summer%' THEN 'Summer Work'
+	WHEN Visit.Description LIKE 'Water Tre%' THEN 'Water Treatment'
+	WHEN Visit.Description LIKE '%Winter%' THEN 'Winter Work'
+	ELSE 'Inspection'
+  END AS Description
+
+FROM
+  dbo.Z_Wennsoft_export_service_contracts AS Contracts 
+  LEFT OUTER JOIN
+  (SELECT LTRIM(RTRIM(Contract_Number)) AS Contract_Number, LTRIM(RTRIM(Work_Summary)) AS Description, Visit_Date, Internal_Key, Budget_Hours
+    FROM dbo.SC_CONTRACT_VISIT_MC) AS Visit
+    ON Visit.Contract_Number = Contracts.Contract_number 
+  --LEFT OUTER JOIN
+  --(SELECT Internal_Key, LTRIM(RTRIM(Equipment)) AS Equipment
+  --  FROM dbo.SC_CONTRACT_VISIT_EQUIP_MC) AS VEqp
+  --  ON Visit.Internal_Key = VEqp.Internal_Key
+--WHERE Visit.Description LIKE 'Water Tre%'
+ORDER BY Visit.Visit_Date DESC, Visit.Contract_Number--, VEqp.Equipment
