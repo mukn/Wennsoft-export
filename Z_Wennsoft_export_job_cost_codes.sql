a9905f3fc1a619ffdb3d7969beffe45eb99e0697
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-04-15 13:04:10 -0400

    Update Z_Wennsoft_export_job_cost_codes.sql
    Corrected the labor bucket.

--- Z_Wennsoft_export_job_cost_codes.sql
+++ Z_Wennsoft_export_job_cost_codes.sql
@@ -1,10 +1,12 @@
 SELECT
 	j1.Job_Number
 	,j1.CC_Number
+	,j1.CC_Description
 	,j1.CC_ElementNumber
 	,j1.CC_ElementCode
+	,2 AS Profit_Type
 	,CASE
-		WHEN j1.CC_Number IN('01100-FIELD-0FRMN-00000' -- Labor
+		WHEN j1.CC_Number IN('01200-FIELD-0JRMN-00000' -- Labor
 			,'02100-0MATL-00000-00000' -- Material
 			,'03000-000EQ-00PUR-00000' -- Purchased equipment
 			,'03100-000EQ-00RNT-00000' -- Rental equipment
@@ -14,9 +16,9 @@ SELECT
 			)
 			THEN j2.Original_Est_Cost
 		ELSE 0
-	END AS Original_Est_Cost_Calc
+	END AS Original_Est_Cost
 	,CASE
-		WHEN j1.CC_Number IN('01100-FIELD-0FRMN-00000' -- Labor
+		WHEN j1.CC_Number IN('01200-FIELD-0JRMN-00000' -- Labor
 			,'02100-0MATL-00000-00000' -- Material
 			,'03000-000EQ-00PUR-00000' -- Purchased equipment
 			,'03100-000EQ-00RNT-00000' -- Rental equipment
@@ -26,7 +28,7 @@ SELECT
 			)
 			AND (j2.Original_Est_Hours > 0)
 			THEN j2.Original_Est_Hours
-		WHEN j1.CC_Number IN('01100-FIELD-0FRMN-00000' -- Labor
+		WHEN j1.CC_Number IN('01200-FIELD-0JRMN-00000' -- Labor
 			,'02100-0MATL-00000-00000' -- Material
 			,'03000-000EQ-00PUR-00000' -- Purchased equipment
 			,'03100-000EQ-00RNT-00000' -- Rental equipment
@@ -37,13 +39,27 @@ SELECT
 			AND (j2.Original_Est_Cost = 0 )
 			THEN 0
 		ELSE 1
-	END AS Original_Est_Hours_Calc
-	,j2.Original_Est_Cost AS Est_cost
-	,j2.Original_Est_Hours
-	,j2.Original_Est_Quantity
+	END AS Original_Est_Hours
 	,CASE
-		WHEN Original_Est_Hours > 0 THEN Original_Est_Hours
-		ELSE 0
+		WHEN j1.CC_Number IN('01200-FIELD-0JRMN-00000' -- Labor
+			,'02100-0MATL-00000-00000' -- Material
+			,'03000-000EQ-00PUR-00000' -- Purchased equipment
+			,'03100-000EQ-00RNT-00000' -- Rental equipment
+			,'06000-0SUBC-00000-00000' -- Subcontractors
+			,'07100-0BRDB-00XDC-00000' -- Burden
+			,'08000-0CNTG-00000-00000' -- Contingency
+			)
+			AND j2.Original_Est_Hours > 0 THEN j2.Original_Est_Hours
+		WHEN j1.CC_Number IN('01200-FIELD-0JRMN-00000' -- Labor
+			,'02100-0MATL-00000-00000' -- Material
+			,'03000-000EQ-00PUR-00000' -- Purchased equipment
+			,'03100-000EQ-00RNT-00000' -- Rental equipment
+			,'06000-0SUBC-00000-00000' -- Subcontractors
+			,'07100-0BRDB-00XDC-00000' -- Burden
+			,'08000-0CNTG-00000-00000' -- Contingency
+			)
+			AND j2.Original_Est_Cost = 0 THEN 0
+		ELSE 1
 	END AS Update_Qty
 FROM
 	Z_Wennsoft_jobs_with_all_cost_codes AS j1
@@ -55,7 +71,7 @@ FROM
 		AND j1.CC_ElementCode = j2.Cost_Type
 
 
---WHERE j1.CC_Number IN('01100-FIELD-0FRMN-00000' -- Labor
+--WHERE j1.CC_Number IN('01200-FIELD-0JRMN-00000' -- Labor
 --			,'02100-0MATL-00000-00000' -- Material
 --			,'03000-000EQ-00PUR-00000' -- Purchased equipment
 --			,'03100-000EQ-00RNT-00000' -- Rental equipment
