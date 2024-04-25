229711c48542806c5c2186b34d23dcb1d70a9e3f
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-04-25 15:48:46 -0400

    Update Get-TechnicianTime.ps1
    Added to the query.

--- TimeReporting/Get-TechnicianTime.ps1
+++ TimeReporting/Get-TechnicianTime.ps1
@@ -15,4 +15,13 @@ param(
 
 
 # Query the database
-$qry = "SELECT Technician_Long_Name,SUM(TRX_Total1) FROM csvw_TT_Audit_Union WHERE (EMPLOYID = '$EmpID') AND (WS_Transaction_Date = $Date)"
+$sqlSrv = "k2a.nacgroup.com"
+$sqlDb = "NAC"
+$qry = @"
+  SELECT Technician_Long_Name,SUM(TRX_Total1) AS Total_Time
+  FROM csvw_TT_Audit_Union 
+  WHERE 
+    (EMPLOYID = '$EmpID')
+    AND (WS_Transaction_Date = $Date)
+  GROUP BY Technician_Long_Name
+  "@
