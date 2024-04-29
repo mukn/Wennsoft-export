c486677aeb466ac1df823afbfb5fb2dfff27ba40
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-04-29 14:21:57 -0400

    Update Z_Wennsoft_export_customer_notes.sql
    Added NOLOCK clause.

--- Z_Wennsoft_export_customer_notes.sql
+++ Z_Wennsoft_export_customer_notes.sql
@@ -5,8 +5,8 @@ SELECT
 	,CNotes.Notes_Topic AS Notes_Service_Index
 	,udf_StripHtml(CNotes.Notes_Text) AS Notes_Text
 FROM 
-	Z_K2A_EXPORT_CUSTOMERS AS C
+	Z_K2A_EXPORT_CUSTOMERS AS C WITH (NOLOCK)
 	LEFT OUTER JOIN
-	CR_CUSTOMER_NOTES_MASTER_MC AS CNotes
+	CR_CUSTOMER_NOTES_MASTER_MC AS CNotes WITH (NOLOCK)
 		ON C.Customer_Code = LTRIM(RTRIM(CNotes.Customer_Code))
 WHERE CNotes.Notes_Text <> ''
