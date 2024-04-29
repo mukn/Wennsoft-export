ce5c6fe182787c944d9c3a7cf3a103dd8b8ff4af
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-04-29 13:46:24 -0400

    Create Z_Wennsoft_customer_notes.sql
    Initial commit.

---
+++ Z_Wennsoft_customer_notes.sql
@@ -0,0 +1,10 @@
+SELECT
+	C.Customer_Code
+	,'C' AS Note_Type
+	,CNotes.Notes_Topic AS Notes_Service_Index
+	,CNotes.Notes_Text AS Notes_Text
+FROM 
+	Z_K2A_EXPORT_CUSTOMERS AS C
+	LEFT OUTER JOIN
+	CR_CUSTOMER_NOTES_MASTER_MC AS CNotes
+		ON C.Customer_Code = LTRIM(RTRIM(CNotes.Customer_Code))
