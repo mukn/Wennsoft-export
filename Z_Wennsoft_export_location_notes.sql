4868c0b63bfb58a302d8e2a8cf49361a57e07d85
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-04-29 13:59:34 -0400

    Create Z_Wennsoft_export_location_notes.sql
    Initial commit.

---
+++ Z_Wennsoft_export_location_notes.sql
@@ -0,0 +1,11 @@
+SELECT
+	L.Customer_Code
+	,L.Location_Code
+	,LNotes.Notes_Topic AS Notes_Service_Index
+	 ,dbo.udf_StripHTML(LNotes.Notes_Text) AS Notes_Text
+FROM
+	Z_Wennsoft_export_locations_union AS L
+	LEFT OUTER JOIN
+	WO_SITE_NOTES_MASTER_MC AS LNotes
+		ON L.Location_Code = LTRIM(RTRIM(LNotes.Notes_Key))
+WHERE LNotes.Notes_Text <> ''
