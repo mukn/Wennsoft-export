a710be425deaed622edcec73effbcf0db71ad13b
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-05-06 14:13:56 -0400

    Update Z_Wennsoft_export_purchase_orders_staging_2_jobs.sql
    Update labels and changes based on ..._staging_1 changes.

--- Z_Wennsoft_export_purchase_orders_staging_2_jobs.sql
+++ Z_Wennsoft_export_purchase_orders_staging_2_jobs.sql
@@ -1,13 +1,13 @@
 SELECT
   P1.PO_Number
-  ,P1.Line_Number
+  ,CAST(P1.Line_Number AS int) AS Line_Number
   ,P1.Vendor_Code
   ,P1.Document_Date
   ,P1.PO_Type
-  ,2 AS Product_Indicator
+  ,3 AS Product_Indicator
   ,P1.Work_Number
   ,P1.GL_Account
-  ,J1.State
+  ,W1.Bill_State AS State
   ,GL.GL_Wennsoft
   ,GL.GL_Wennsoft_Description
   ,GL.CostCodes_Wennsoft
@@ -18,13 +18,13 @@ SELECT
 FROM
   dbo.Z_Wennsoft_export_purchase_orders_staging_1 AS P1 
   INNER JOIN
-  (SELECT LTRIM(RTRIM(Job_Number)) AS jn, State, Status_Code
-  FROM dbo.JC_JOB_MASTER_MC WITH (NOLOCK)
-  WHERE Status_Code = 'A') AS J1
-    ON P1.Work_Number = J1.jn
+  (SELECT LTRIM(RTRIM(WO_Number)) AS WO_Number, Bill_State
+  FROM dbo.WO_HEADER_MC WITH (NOLOCK)
+  WHERE  Dispatch_Status_Code <> 'F') AS W1
+    ON P1.Work_Number = W1.WO_Number
   LEFT OUTER JOIN
   dbo.Z_Wennsoft_GL_map AS GL
-    ON P1.GL_Account = GL.GL_Current_Numeric AND J1.State = GL.State
+    ON P1.GL_Account = GL.GL_Current_Numeric AND W1.Bill_State = GL.State
 WHERE
-  (P1.GL_Account LIKE '06%') OR (P1.GL_Account LIKE '04%')
-ORDER BY P1.PO_Number, P1.Line_Number
+  (P1.GL_Account LIKE '06%') OR (P1.GL_Account LIKE '04%') AND (p1.Document_Date > DATENAME(YEAR,GETDATE()))
+ORDER BY P1.PO_Number, CAST(P1.Line_Number AS int)
